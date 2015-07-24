995081e48ad074953b1fb2cc82fe9e8c05a3fca0
vmehta <vmehta@infinite-usa.com>
2015-07-24 15:54:50 +0530

    integrated autofac with many console

---
+++ AutofacSolution/ManyConsole.Integrate/ErrorLogCommand.cs
@@ -0,0 +1,66 @@
+﻿using Autofac;
+using Autofac.Interface;
+using Autofac.Interface.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManyConsole.Integrate
+{
+    public class ErrorLogCommand : ConsoleCommand
+    {
+        private static IContainer container;
+        public string _error;
+        public ErrorLogCommand()
+        {
+            IsCommand("log");
+
+            HasOption("e|error=", "enter the error", error => _error = error);
+            HasOption("s|sql", "select sql as a provider", x => RegisterSQL());
+            HasOption("f|file", "select file as a provider", x => RegisterFile());
+
+        }
+
+        private void RegisterSQL()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<SQLLogError>().As<IErrorLogService>();
+            container = builder.Build();
+        }
+
+        private void RegisterFile()
+        {
+            var builder = new ContainerBuilder();
+            builder.RegisterType<FileLogError>().As<IErrorLogService>();
+            container = builder.Build();
+        }
+
+        public override int Run(string[] remainingArguments)
+        {
+            if (string.IsNullOrWhiteSpace(_error))
+            {
+                Console.WriteLine("You did not specify error");
+            }
+            else
+            {
+                WriteErrorLog(_error);
+                Console.WriteLine("Your error is: " + _error);
+            }
+
+            //Console.WriteLine("The remaining arguments were " + Newtonsoft.Json.JsonConvert.SerializeObject(remainingArguments));
+
+            return 0;
+        }
+
+        private static bool WriteErrorLog(string _error)
+        {
+            using (var scope = container.BeginLifetimeScope())
+            {
+                var writer = scope.Resolve<IErrorLogService>();
+                return writer.LogError(_error, DateTime.Now);
+            }
+        }
+    }
+}
---
+++ AutofacSolution/ManyConsole.Integrate/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ManyConsole.Integrate
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var commands = GetCommands();
+
+            // then run them.
+            ConsoleCommandDispatcher.DispatchCommand(commands, args, Console.Out);
+            Console.ReadLine();
+        }
+
+        public static IEnumerable<ConsoleCommand> GetCommands()
+        {
+            return ConsoleCommandDispatcher.FindCommandsInSameAssemblyAs(typeof(Program));
+        }
+    }
+}
